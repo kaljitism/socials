27fea303d72286145dd97ef1d577721ffc66e9ae
kaljitism <dwikaljit@gmail.com>
2024-09-03 16:08:39 +0530

    JSON Serializable Generated Output

---
+++ shared/lib/src/models.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserImpl _$$UserImplFromJson(Map<String, dynamic> json) => _$UserImpl(
+      id: json['id'] as String?,
+      email: json['email'] as String,
+    );
+
+Map<String, dynamic> _$$UserImplToJson(_$UserImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+    };
